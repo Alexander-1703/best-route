5c38c112a30f59a11f7c96ac18b518ba19178b88
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2023-01-10 15:51:48 +0300

    initial commit

---
+++ src/main/java/io/proj3ct/BestRouteBot/BestRouteBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.BestRouteBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestRouteBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestRouteBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3ct/BestRouteBot/BestRouteBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.BestRouteBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestRouteBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
